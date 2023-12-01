9d654d9312db51e534388b218698d67f562853c6
kulot-tin <nits.rodriguez10@gmail.com>
2023-12-01 20:54:11 +0800

    chart

--- index.php
+++ index.php
@@ -14,12 +14,15 @@
     <!--cnd of adminLTE-->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <!-- CDN of Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 
 </head>
 <body>
             <!--sidebar-->
             <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="position:fixed;top:0,left:0;height:100%overflow-y:auto;">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
     <img src="ako.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
@@ -83,7 +86,7 @@
   <!-- /.sidebar -->
 </aside>
             <!-- Navbar -->
-            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light layout-fixed fixed-top">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -212,39 +215,64 @@
                     class="fas fa-th-large"></i></a>
                 </li>
             </ul>
-            </nav>
-
-            <div>
-                <canvas id="myChart"></canvas>
-              </div>
-
-              <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-              <script>
-                const ctx = document.getElementById('myChart');
-
-                new Chart(ctx, {
-                  type: 'bar',
-                  data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                      label: '# of Votes',
-                      data: [12, 19, 3, 5, 2, 3],
-                      borderWidth: 1
-                    }]
-                  },
-                  options: {
-                    scales: {
-                      y: {
-                        beginAtZero: true
-                      }
-                    }
-                  }
-                });
-              </script>
- 
-  
+            </nav> 
             <!-- /.navbar -->
+
+    <!-- chart -->
+        <div class="container mt-5">
+            <canvas id="myChart" width="300" height="200"></canvas>
+        </div>
+    <!-- Add the JavaScript code for the chart -->
+    <script>
+        // Sample data for the bar chart
+        const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Data Set',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
+        };
+
+        // Configuration options
+        const options = {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        };
+
+        // Get the canvas element
+        const ctx = document.getElementById('myChart').getContext('2d');
+
+        // Create the bar chart
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: options
+        });
+    </script>
+
 
 </body>
 </html>
